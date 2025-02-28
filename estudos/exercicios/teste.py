8639fb6471feb2e0f3f72d653589547e19f168a8
Alan Neves <alan.n.ribeiro257@hotmail.com>
2025-02-28 16:35:53 -0300

    update

--- estudos/exercicios/teste.py
+++
@@ -1,7 +0,0 @@
-print(f"Soma: 5 + 3 = {5 + 3}, Subtração: 10 - 7 = {10 - 7}, Multiplicação: 4 x 2 = {4 * 2}, Divisão: 16 / 4 = {16 / 4}")
-
-soma = () + ()
-sub = () - ()
-mult = () * ()
-div = () / ()
-
